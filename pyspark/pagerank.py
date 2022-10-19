c3df5a30fcf6c042bae69b12f6de844f97a20889
Oswaldo Jimenez <oswaldo-andres.jimenez-hidalgo@etu.univ-nantes.fr>
2022-10-19 13:24:50 +0200

    Fix partitioning

--- pyspark/pagerank.py
+++ pyspark/pagerank.py
@@ -25,20 +25,24 @@ bin/spark-submit examples/src/main/python/pagerank.py data/mllib/pagerank_data.t
 import re
 import sys
 from operator import add
-from typing import Iterable, Tuple
+import time
+import json
+from subprocess import call
 
-from pyspark.resultiterable import ResultIterable
 from pyspark.sql import SparkSession
 
 
-def computeContribs(urls: ResultIterable[str], rank: float) -> Iterable[Tuple[str, float]]:
+# removed typing for compatibility with Spark 3.1.3
+# typing ok with spark 3.3.0
+
+def computeContribs(urls, rank) :
     """Calculates URL contributions to the rank of other URLs."""
     num_urls = len(urls)
     for url in urls:
         yield (url, rank / num_urls)
 
 
-def parseNeighbors(urls: str) -> Tuple[str, str]:
+def parseNeighbors(urls) :
     """Parses a urls pair string into urls pair."""
     parts = re.split(r'\s+', urls)
     return parts[0], parts[2]
@@ -52,6 +56,12 @@ if __name__ == "__main__":
     print("WARN: This is a naive implementation of PageRank and is given as an example!\n" +
           "Please refer to PageRank implementation provided by graphx",
           file=sys.stderr)
+
+    statistics_data = {}
+    start_program_timestamp = time.time()
+    partition = True
+    compute_max = True
+    gs_bucket = "gs://pagerank"
 
     # Initialize the spark context.
     spark = SparkSession\
@@ -72,6 +82,10 @@ if __name__ == "__main__":
     # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
     ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
 
+    if (partition):
+        links = links.partitionBy(numPartitions = None) #using default portable hash and default number of partitions
+        ranks = ranks.partitionBy(numPartitions = None)
+
     # Calculates and updates URL ranks continuously using PageRank algorithm.
     for iteration in range(int(sys.argv[2])):
         # Calculates URL contributions to the rank of other URLs.
@@ -83,7 +97,23 @@ if __name__ == "__main__":
         ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
 
     # Collects all URL ranks and dump them to console.
-    for (link, rank) in ranks.collect():
-        print("%s has rank: %s." % (link, rank))
+    time_tag = time.strftime("%Y%m%d-%H%M%S")
+    ranks.coalesce(1).saveAsTextFile(gs_bucket+"/pyspark_result_"+time_tag) #colaesce needed to have single partition write access
+    if (compute_max):
+        top_pagerank = ranks.sortBy(lambda x : x[1],False).toDF().limit(10).rdd #top 10 pagerank only
+        top_pagerank.coalesce(1).saveAsTextFile(gs_bucket+'/top_pagerank_'+time_tag) #colaesce needed to have single partition write access
 
     spark.stop()
+    finish_program_timestamp = time.time()
+    statistics_data['start_program_timestamp'] = start_program_timestamp
+    statistics_data['finish_program_timestamp'] = finish_program_timestamp
+    statistics_data['total_time_elapsed'] = finish_program_timestamp - start_program_timestamp
+
+    #for (link, rank) in ranks.collect():
+    #    print("%s has rank: %s." % (link, rank))
+
+    statistics_filename = 'pyspark_statistics'+time_tag+'.json'
+    with open(statistics_filename, 'w+') as outfile:
+        outfile.write(json.dumps(statistics_data))
+
+    call(["gsutil" ,"cp",statistics_filename ,gs_bucket])
