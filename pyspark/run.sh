fecc21da48b9dca22fc3ef8d49985df13d1b0391
Oswaldo Jimenez <oswaldo-andres.jimenez-hidalgo@etu.univ-nantes.fr>
2022-10-19 16:14:18 +0200

    Clean pyspark runnable file

--- pyspark/run.sh
+++ pyspark/run.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-## En local ->
-## pig -x local -
-
-## en dataproc...
-
 ## copy data
-gsutil cp small_page_links.nt gs://myown_bucket/
+gsutil cp small_page_links.nt gs://pagerank/
 
-## copy pig code
-gsutil cp pagerank-notype.py gs://myown_bucket/
+## copy PySpark code
+gsutil cp pagerank.py gs://pagerank/
 
-## Clean out directory
-gsutil rm -rf gs://myown_bucket/out
-
-
-## create the cluster
+## create the cluster 2 workers
 gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project master-2-large-scale-data
-
 
 ## run
 ## (suppose that out directory is empty !!)
-gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-a35a gs://myown_bucket/pagerank-notype.py  -- gs://myown_bucket/small_page_links.nt 3
+gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-a35a gs://pagerank/pagerank.py -- gs://public_lddm_data/page_links_en.nt.bz2 3
+
 
 ## access results
-gsutil cat gs://myown_bucket/out/pagerank_data_10/part-r-00000
+gsutil cat gs://pagerank/pyspark_result_*
+gsutil cat gs://pagerank/top_pagerank_*
 
 ## delete cluster...
 gcloud dataproc clusters delete cluster-a35a --region europe-west1
-
